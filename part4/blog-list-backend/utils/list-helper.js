1e0f1a98f6ca378f5dfe6955196a9ecc6de42659
nowaylifer <nowaylifer@gmail.com>
2023-09-25 10:41:33 +0300

    feat: create list helpers with tests

---
+++ part4/blog-list-backend/utils/list-helper.js
@@ -0,0 +1,57 @@
+const dummy = (blogs) => 1;
+
+const calcTotalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const getFavoriteBlog = (blogs) => {
+  return blogs.reduce((result, current) => (result.likes < current.likes ? current : result));
+};
+
+const getAuthorWithMostBlogs = (blogs) => {
+  const blogMap = blogs.reduce((acc, blog) => {
+    const { author } = blog;
+
+    if (author in acc) {
+      acc[author]++;
+    } else {
+      acc[author] = 1;
+    }
+
+    return acc;
+  }, {});
+
+  const resultAuthor = Object.keys(blogMap).reduce((res, cur) =>
+    blogMap[cur] > blogMap[res] ? cur : res
+  );
+
+  return { author: resultAuthor, blogs: blogMap[resultAuthor] };
+};
+
+const getAuthorWithMostLikes = (blogs) => {
+  const likeMap = blogs.reduce((acc, blog) => {
+    const { author, likes } = blog;
+
+    if (author in acc) {
+      acc[author] += likes;
+    } else {
+      acc[author] = likes;
+    }
+
+    return acc;
+  }, {});
+
+  const resultAuthor = Object.keys(likeMap).reduce((res, cur) =>
+    likeMap[cur] > likeMap[res] ? cur : res
+  );
+
+  return { author: resultAuthor, likes: likeMap[resultAuthor] };
+};
+
+module.exports = {
+  dummy,
+  calcTotalLikes,
+  getFavoriteBlog,
+  getAuthorWithMostBlogs,
+  getAuthorWithMostLikes,
+};
