fd4655fa644d45a7f46efa03d2a9335da3ad4031
nowaylifer <nowaylifer@gmail.com>
2023-10-02 16:52:34 +0300

    chore: install react-testing library

--- part5/bloglist-frontend/.eslintrc.cjs
+++ part5/bloglist-frontend/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, 'jest/globals': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -12,10 +12,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/prop-types': false
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/prop-types': false,
   },
-}
+};
